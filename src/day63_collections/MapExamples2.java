b107053129aee968a14513d847585638c112cb17
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-07-18 13:19:07 -0500

    Collections 2

---
+++ src/day63_collections/MapExamples2.java
@@ -0,0 +1,14 @@
+package day63_collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapExamples2 {
+    public static void main(String[] args) {
+
+        // id, Person
+        Map<Integer, Person> map = new HashMap<>();
+
+
+    }
+}
