7dbcea6d7d914d046e5543790e99b7271e4e3f5e
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-07-18 14:25:40 -0500

    Collections 2

--- src/day63_collections/MapExamples2.java
+++ src/day63_collections/MapExamples2.java
@@ -33,5 +33,38 @@ public class MapExamples2 {
         map3.put(-12 , new Person("Nick", 10));
         System.out.println(map3);
 
+
+        /*
+            DB example
+
+
+            FIRST_NAME James
+            LAST_NAME Bond
+            AGE       50
+
+            FIRST_NAME Amy
+            LAST_NAME Rose
+            AGE       30
+
+            Map<String, String> map = new HashMap<>();
+            map.put("FIRST_NAME", "James);
+            map.put("LAST_NAME", "Bond");
+
+            map.get("FIRST_NAME)
+
+           if( map.get("AGE") < 0 ) fail test case
+
+           if( map.get("FIRST_NAME") == null) fail test case
+
+           List< Map<String, String> > list
+
+                list.get(0) -> Map<String, String> (james bond)
+                list.get(0).get("FIRST_NAME") -> james
+
+         */
+
+
+
+
     }
 }
