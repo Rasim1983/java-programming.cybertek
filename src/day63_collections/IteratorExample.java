460203921430dd3f51b9f53f26bda2ec90d61ebe
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-07-18 15:53:46 -0500

    Collections 2

---
+++ src/day63_collections/IteratorExample.java
@@ -0,0 +1,15 @@
+package day63_collections;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class IteratorExample {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+
+
+
+    }
+}
